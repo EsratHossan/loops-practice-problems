9e8a02d50b2554d480ab784599717f91c8b96a8c
EsratHossan <esrathossan7@gmail.com>
2023-02-03 21:58:42 +0600

    added revision

---
+++ revision.js
@@ -0,0 +1,28 @@
+// String type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+// Number type variable
+var sunglassPrice = 120;
+
+// boolean type variable
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = tableItems[3];
+// set replace index
+tableItems[4]= 'notebook';
+var penIndex = tableItems.indexOf('pen');
+// push pop
+
+// conditionals
+if(tableItems.length > 4){
+
+}
+else if(tableItems[3] == 'pen'){
+
+}
+else{
+    
+}

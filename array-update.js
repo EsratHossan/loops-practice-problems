be01d7b4be84bf4d3713663ecdb3fe77db9cc44e
EsratHossan <esrathossan7@gmail.com>
2023-02-04 12:32:15 +0600

    added array-update largest-number isosceles

---
+++ array-update.js
@@ -0,0 +1,10 @@
+// Practice Problem 01 array
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+var bananaIndex = fruits.indexOf('Banana');
+fruits[bananaIndex] = 'Mango';
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
